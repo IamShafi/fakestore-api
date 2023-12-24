ef4ffa85267b427c66b42b5bbf8faa8edbc80b2b
iamShafi <67453945+IamShafi@users.noreply.github.com>
2023-12-24 18:04:42 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 import Home from './pages/Home'
 import ProductDetails from'./pages/ProductDetails'
-import Sidebar from './pages/Sidebar'
-import Header from './pages/Header'
-import Footer from './pages/Footer'
+// import Sidebar from './pages/Sidebar'
+// import Header from './pages/Header'
+// import Footer from './pages/Footer'
 
 const App = () => {
   return <div className='overflow-hidden'>
     <Router>
-      <Header />
-      <Sidebar />
+      {/* <Header />
+      <Sidebar /> */}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/product/:id' element={<ProductDetails />} />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </Router>
   </div>;
 };
