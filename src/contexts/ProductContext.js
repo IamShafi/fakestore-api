73381bc2857633a61ad4fe7c093832f8027c07b7
iamShafi <67453945+IamShafi@users.noreply.github.com>
2023-12-24 20:49:21 +0600

    completed product item fetch

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -10,9 +10,11 @@ const ProductProvider = ({children}) => {
     const fetchProducts = async () => {
       const response = await fetch('https://fakestoreapi.com/products');
       const data = await response.json();
-    }
-  })
-  return <ProductContext.Provider value={{}}>
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+  return <ProductContext.Provider value={{products}}>
     {children}
   </ProductContext.Provider>;
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,33 @@
 import React from 'react';
+// import context
+import {ProductContext} from '../contexts/ProductContext';
+//import components
+import Product from '../components/Product'
 
 const Home = () => {
-  return <div>Homepage</div>;
+  // get products from product context
+  const {products} = React.useContext(ProductContext);
+  // get only selected category items
+  const filteredProducts = products.filter((item) =>{
+    return (
+      item.category === "men's clothing" || item.category === "women's clothing"
+    );
+  })
+
+  return (
+    <div>
+      <section className='py-16'>
+        <div className='container mx-auto'>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px]
+          max-w-sm mx-auto md:max-w-none md:mx-0'>
+            {filteredProducts.map((item) => (
+              <Product key={item.id} product={item} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </div>
+  )
 };
 
 export default Home;
